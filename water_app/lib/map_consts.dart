5c0a951264c60ae94502223c182d67a5e375a888
walkerhsu <walkerhsu0808@gmail.com>
2023-09-24 18:26:52 +0800

    [flutter] add a map using flutter_map

---
+++ water_app/lib/map_consts.dart
@@ -0,0 +1,10 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken =
+      "pk.eyJ1Ijoid2Fsa2VyaHN1IiwiYSI6ImNsbXd6N2FkdTBsdGEybHBoM2Riajh3aXcifQ.u2DBinAPW6TC9kdS-lJW8A";
+
+  static const String mapBoxStyleId ="clmx0qvh1023701r940xsdpnb";
+
+  static const myLocation = LatLng(51.5090214, -0.1982948);
+}
