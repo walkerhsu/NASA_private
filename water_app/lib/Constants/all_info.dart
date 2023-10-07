e7cf38aa8453e406bd9b096924794f13589cc7e3
walkerhsu <walkerhsu0808@gmail.com>
2023-10-07 17:55:05 +0800

    :sparkles: [flutter] menu book

---
+++ water_app/lib/Constants/all_info.dart
@@ -0,0 +1,17 @@
+import 'package:water_app/processData/process_species.dart';
+import 'package:water_app/processData/process_stations.dart';
+
+class AllInfo {
+  static Map<String, dynamic> allSpecies = {
+    "Taiwan": ProcessSpecies.TaiwanSpecies,
+    "Canada": ProcessSpecies.CanadaSpecies,
+    // "America": ProcessSpecies.AmericaSpecies,
+  };
+
+  static Map<String, dynamic> allStations = {
+    "Taiwan": ProcessStations.taiwanStationData,
+    "Canada": ProcessStations.CanadaStationData,
+    // "America": ProcessStations.AmericaStationData,
+  };
+  
+}
---
+++ water_app/lib/Constants/map_consts.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:latlong2/latlong.dart';
+
+class MapConstants {
+  static const String mapBoxAccessToken =
+      "pk.eyJ1IjoibWFya3ltYXJrbGVlIiwiYSI6ImNsbmV2MmJpczBnYmgydHBkZ2l5czRmMGwifQ.lHpfBNNYv6tWDfhiJWHhNA";
+  static const String mapBoxStyleId = "clnfny2al01j001ps7ep1g539";
+  static const  myLocation = {
+    "Taiwan": LatLng(25.03472364001052, 121.56517519621646),
+    "America": LatLng(56.0451222,-130.040261),
+    "Canada": LatLng(49.4, -114.35),
+  };
+}
