5f97c49a0f87189062faae859b03a878eeecb01a
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2023-10-08 15:06:48 +0800

    :lipstick: update all pages features 90% done

--- water_app/lib/Constants/all_info.dart
+++ water_app/lib/Constants/all_info.dart
@@ -1,3 +1,5 @@
+import 'package:csv/csv.dart';
+import 'package:water_app/Storage/cloud_storage.dart';
 import 'package:water_app/processData/process_species.dart';
 import 'package:water_app/processData/process_stations.dart';
 
@@ -14,4 +16,9 @@ class AllInfo {
     // "America": ProcessStations.AmericaStationData,
   };
   
+  // static Future<List<Map<String, dynamic>>> getMenus(context, String country) async {
+  //   String menuCSVString = await CloudStorage.getRawtxtData("ill_book.csv");
+  //   List<List<dynamic>> processMenuData = CsvToListConverter().convert(menuCSVString, eol: "\n");
+  //   List<Map<String, dynamic>> menuData = [];
+  // } 
 }
